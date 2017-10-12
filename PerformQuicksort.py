d2a8b2aff6f70782ed5c41d6f592b901c4b123f4
Jan Feys <feys.jan@gmail.com>
2017-10-11 23:08:32 -0400

    Optimized if-loops

--- PerformQuicksort.py
+++ PerformQuicksort.py
@@ -29,47 +29,33 @@ def partition_array(a):
             i = i+1
 
     a[0], a[i-1]= a[i-1], a[0]
-    return a, i-1
+    return i-1
 
 def quick_sort(a,pivot_option):
     #Quicksort the array a by first choosing a pivot, then partitioning the array and finally recursively calling quicksort on the left and right partitions.
     
     if (len(a)==1):
         return a
+    
     pivot_index = choose_pivot(a,pivot_option)
-
     a[0], a[pivot_index] = a[pivot_index], a[0]
-    a, pivot_position = partition_array(a)
+    pivot_position = partition_array(a)
 
-    if (pivot_position == 0):
-        la = a[0:1]
-    else:
+    if (pivot_position != 0):
         la = a[0:pivot_position]
-
-    if (pivot_position == len(a)-1):
-        ra = a[pivot_position:]
-    else:
-        ra = a[pivot_position+1:]
-
-    la = quick_sort(la,pivot_option)
-    ra = quick_sort(ra,pivot_option)
-
-    if (pivot_position == 0):
-        a[0:1] = la
-    else:
+        la = quick_sort(la,pivot_option)
         a[0:pivot_position] = la
 
-    if (pivot_position == len(a)-1):
-        a[pivot_position:] = ra
-    else:
+    if (pivot_position != len(a)-1):
+        ra = a[pivot_position+1:]
+        ra = quick_sort(ra,pivot_option)
         a[pivot_position+1:]= ra
     return a
 
 if __name__ == "__main__":
-    length_a = 1000
-    range_a = 10000
+    length_a = 200
+    range_a = 300
     a=[randint(0,range_a) for _ in range(0,length_a)]
-    
     a_sorted = quick_sort(a,'median-of-three')
     print(a_sorted)
 
